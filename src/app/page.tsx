f46f328d4d4b99908cf7a39444cace39441277e7
Joy Elohor Onovae <148342219+joyonovae@users.noreply.github.com>
2026-05-07 19:41:24 +0100

    become a driver cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -176,6 +176,11 @@ export default function HomePage() {
                 Car Rentals
               </Link>
 
+              <Link
+              href="/offer-a-ride"
+              className="rounded-full bg-[#061116] px-5 py-3 text-sm font-bold text-white transition hover:-translate-y-1 hover:bg-emerald-600 sm:px-6">
+              Become a Driver
+            </Link>
               
             </div>
           </div>
